de6ff11afc225dfe7251a915b749bd3e20b0c7af
yan5751839 <yan5751839@gmail.com>
2022-06-01 14:42:47 +0200

    init

---
+++ pages/api/checkout_sessions/[id].ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: '2020-08-27',
+})
+
+export default async function handler (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const id: string = req.query.id as string
+  try {
+    if (!id.startsWith('cs_')) {
+      throw Error('Incorrect CheckoutSession ID.')
+    }
+    const checkout_session: Stripe.Checkout.Session =
+      await stripe.checkout.sessions.retrieve(id, {
+        expand: ['payment_intent'],
+      })
+
+    res.status(200).json(checkout_session)
+    console.log('7346 !!!!????')
+    fetch('https://gorest.co.in/public/v2/users')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+      })
+  } catch (err) {
+    const errorMessage =
+      err instanceof Error ? err.message : 'Internal server error'
+    res.status(500).json({ statusCode: 500, message: errorMessage })
+  }
+}
